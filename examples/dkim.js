8e4f235a99fbfe28bd01900ecc2b4bc74cbd194f
Erin Dachtler <download333@gmail.com>
2016-12-04 19:55:48 -0800

    Added DKIM example

---
+++ examples/dkim.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var sendmail = require('../sendmail')({
+  silent:true,
+  dkim: {
+    privateKey: fs.readFileSync('dkim-private.pem', 'utf-8'),
+    keySelector: 'mydomainkey'
+  }
+});
+
+sendmail({
+    from: 'test@yourdomain.com',
+    to: 'info@yourdomain.com',
+    replyTo: 'jason@yourdomain.com',
+    subject: 'MailComposer sendmail',
+    html: 'Mail of test sendmail ',
+  }, function(err, reply) {
+    console.log(err && err.stack);
+    console.dir(reply);
+});
